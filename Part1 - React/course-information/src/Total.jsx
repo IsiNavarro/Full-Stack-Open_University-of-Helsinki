a8563b89bf0ae8baa2187b9dc4fcdfda44e12184
IsiNavarro <99091821+IsiNavarro@users.noreply.github.com>
2026-05-31 12:55:34 +0200

    Update Total. forEach loop

--- Part1 - React/course-information/src/Total.jsx
+++ Part1 - React/course-information/src/Total.jsx
@@ -1,5 +1,11 @@
-const Total = (promts) => {
-    return <>
-    </>
+const Total = (props) => {
+    console.log(props.parts)
+
+    let totalExercises = 0
+
+    props.parts.forEach(part => {
+        totalExercises += part.exercises
+    })
+    return <p>Total exercises: {totalExercises}</p>
 }
 export default Total
