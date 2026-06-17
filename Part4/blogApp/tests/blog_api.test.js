1cc840fcd1b73a98c075d71b238483e5ff402291
IsiNavarro <99091821+IsiNavarro@users.noreply.github.com>
2026-06-17 20:56:43 +0200

    add helper function to tests

--- Part4/blogApp/tests/blog_api.test.js
+++ Part4/blogApp/tests/blog_api.test.js
@@ -4,28 +4,13 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../app')
 const Blog = require('../models/blog')
-const { before } = require('lodash')
+const helper = require('./helper')
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {
-        title: "Title",
-        author: "Some Author",
-        url: "SomeURL",
-        likes: 13
-    },
-    {
-        title: "Second Title",
-        author: "Some Other Author",
-        url: "a different URL",
-        likes: 2
-    }
-]
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(initialBlogs)
+    await Blog.insertMany(helper.initialBlogs)
 })
 
 test('blogs are returned as JSON', async () => {
@@ -33,6 +18,39 @@ test('blogs are returned as JSON', async () => {
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const authors = response.body.map(e => e.author)
+    assert(authors.includes('Some Author'))
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: "new Title",
+        author: "new Author",
+        url: "new URL",
+        likes: 1
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const authors = blogsAtEnd.map(n => n.author)
+    assert(authors.includes("new Author"))
 })
 
 after(async () => {
