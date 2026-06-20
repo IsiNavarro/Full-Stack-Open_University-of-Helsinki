960a0fcc488c6d659a48ba4c197f8cff4a6afeb8
IsiNavarro <99091821+IsiNavarro@users.noreply.github.com>
2026-06-20 20:26:09 +0200

    complete exercise 4.18 implement token-based authentication

--- Part4/blogApp/app.js
+++ Part4/blogApp/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogRouter')
 const userRouter = require('./controllers/userRouter')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
