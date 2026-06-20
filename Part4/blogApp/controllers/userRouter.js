f8104e5d265382301b104b090686e3fd8b80d6d5
IsiNavarro <99091821+IsiNavarro@users.noreply.github.com>
2026-06-20 17:13:23 +0200

    complete exercise 4.15

---
+++ Part4/blogApp/controllers/userRouter.js
@@ -0,0 +1,41 @@
+const userRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+userRouter.post('/', async (request, response) => {
+    const { username, name, password } = await request.body
+
+    if (!password || password.length < 3) {
+        return response.status(400).json({ 
+
+            error: 'password must be at least 3 characters long' 
+        })
+    }
+
+    if (!username || username.length < 3) {
+        return response.status(400).json({
+            error: 'username must be at least 3 characters long'
+        })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+userRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+
+    response.json(users)
+})
+
+module.exports = userRouter
--- Part4/blogApp/utils/middleware.js
+++ Part4/blogApp/utils/middleware.js
@@ -1,9 +1,13 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
+  const logBody = { ...request.body }
+  if (logBody.password) {
+    logBody.password = '********'
+  }
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
+  logger.info('Body:  ', logBody)
   logger.info('---')
   next()
 }
@@ -19,8 +23,9 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    return response.status(400).json({ error: 'expected `username` to be unique' })
   }
-
   next(error)
 }
 
